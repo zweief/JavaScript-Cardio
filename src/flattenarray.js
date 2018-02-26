5f68c7817c86adef68ff382d64fc703c76af0c04
Florian Frenzel <zweief@users.noreply.github.com>
2018-02-26 18:17:17 +0100

    better solution for flatten array function

--- src/flattenarray.js
+++ src/flattenarray.js
@@ -6,6 +6,11 @@
 
 export function flattenArray(arr) {
   let flatArr = [];
-  arr.map(el => flatArr.push(...el))
+  arr.map(el => flatArr.push(...el));
   return flatArr;
 }
+
+// shorte solution
+// function flattenArray(arr) {
+//   return [].concat(...arr);
+// }
